85bcbe40fe1dfad840f9d9885c32801c9de08a3a
Eric Steele <esteele@salesforce.com>
2018-05-24 12:01:58 -0400

    Remove duplicate rule activation

--- sonar-salesforce-pmd/src/main/java/org/sonar/salesforce/rule/SalesforceProfile.java
+++ sonar-salesforce-pmd/src/main/java/org/sonar/salesforce/rule/SalesforceProfile.java
@@ -17,27 +17,91 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
 package org.sonar.salesforce.rule;
 
-import org.sonar.api.profiles.ProfileDefinition;
-import org.sonar.api.profiles.RulesProfile;
-import org.sonar.api.profiles.XMLProfileParser;
-import org.sonar.api.rules.Rule;
-import org.sonar.api.utils.ValidationMessages;
+// import org.sonar.api.profiles.ProfileDefinition;
+// import org.sonar.api.profiles.RulesProfile;
+// import org.sonar.api.profiles.XMLProfileParser;
+// import org.sonar.api.rules.Rule;
+// import org.sonar.api.utils.ValidationMessages;
 
+// import org.sonar.salesforce.SalesforcePlugin;
+
+// import java.io.InputStreamReader;
+
+// public class SalesforceProfile extends ProfileDefinition {
+
+//     @Override
+//     public RulesProfile createProfile(ValidationMessages validation) {
+// 		RulesProfile profile = RulesProfile.create("Salesforce", SalesforcePlugin.LANGUAGE_KEY);
+// 		profile.activateRule(Rule.create(SalesforcePlugin.REPOSITORY_KEY, "GenericSalesforcePmdViolation"), null);
+
+// 		return profile;
+
+//     }
+
+// }
+
+import org.sonar.api.server.profile.BuiltInQualityProfilesDefinition;
 import org.sonar.salesforce.SalesforcePlugin;
+import org.sonar.api.server.rule.RulesDefinition;
+import java.util.Iterator;
+import java.util.List;
 
-import java.io.InputStreamReader;
 
-public class SalesforceProfile extends ProfileDefinition {
+public class SalesforceProfile implements BuiltInQualityProfilesDefinition {
 
-    @Override
-    public RulesProfile createProfile(ValidationMessages validation) {
-		RulesProfile profile = RulesProfile.create("Salesforce", SalesforcePlugin.LANGUAGE_KEY);
-		profile.activateRule(Rule.create(SalesforcePlugin.REPOSITORY_KEY, "GenericSalesforcePmdViolation"), null);
+	public void define(Context context) {
+		NewBuiltInQualityProfile profile = context.createBuiltInQualityProfile("Salesforce Rules", SalesforcePlugin.LANGUAGE_KEY);
+		profile.setDefault(true);
 
-		return profile;
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "GenericSalesforcePmdViolation");
 
-    }
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "IfElseStmtsMustUseBraces");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "WhileLoopsMustUseBraces");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexXSSFromURLParam");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSuggestUsingNamedCred");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidSoslInLoops");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSOQLInjection");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexXSSFromEscapeFalse");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyIfStmt");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessiveClassLength");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexBadCrypto");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestShouldNotUseSeeAllDataTrue");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssMethodCount");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "StdCyclomaticComplexity");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexCSRF");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidLogicInTrigger");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessiveParameterList");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyCatchBlock");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssConstructorCount");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyWhileStmt");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexUnitTestClassShouldHaveAsserts");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ClassNamingConventions");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ForLoopsMustUseBraces");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "CyclomaticComplexity");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexInsecureEndpoint");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyStatementBlock");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidGlobalModifier");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "EmptyTryOrFinallyBlock");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexCRUDViolation");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexOpenRedirect");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexSharingViolations");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDirectAccessTriggerMap");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDmlStatementsInLoops");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "IfStmtsMustUseBraces");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "VariableNamingConventions");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "MethodWithSameNameAsEnclosingClass");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ExcessivePublicCount");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "NcssTypeCount");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "MethodNamingConventions");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidSoqlInLoops");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidHardcodingId");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "AvoidDeeplyNestedIfStmts");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "TooManyFields");
+		profile.activateRule(SalesforcePlugin.REPOSITORY_KEY, "ApexDangerousMethods");
+		profile.done();
+	}
 
 }
